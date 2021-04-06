b031ec0a48d365af8a2b5f88bf971e1211690520
ismail-medhat <faragesmael91@gmail.com>
2021-04-06 19:20:33 +0200

    Inatial 0

---
+++ YT-Livewire/app/Channel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Channel extends Model
+{
+
+    protected $table = 'channels';
+    protected $guarded = [];
+
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function user()
+    {
+
+        return $this->belongsTo(User::class);
+    }
+
+}
